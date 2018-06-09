4083e77870762879d3ffbb435ff34ee2cb656570
Adrian-Mihai <ardelean.adrian.mihai@gmail.com>
2018-06-09 21:30:42 +0300

    change sign_up page's root from /users/sign_up to /sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'pages#index'
 
-  # change the root
+  # change the root devise/sessions#new to login
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+  end
+
+  # change the root devise/registrations#new to sign_up
+  devise_scope :user do
+    get 'sign_up', to: 'devise/registrations#new'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
