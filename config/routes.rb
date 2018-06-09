2158a41c8b4040436f2798744db62979df77725e
Adrian-Mihai <ardelean.adrian.mihai@gmail.com>
2018-06-09 21:07:33 +0300

    fix root change

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#index'
-  
+
   # change the root
-  devise_scope :users do
+  devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
 
